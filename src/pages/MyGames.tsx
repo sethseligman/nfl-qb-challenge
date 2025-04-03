dbc152f7b66f330c826d0f2e5ceffe6ef05a671a
Seth Seligman <sethseligman@gmail.com>
2025-04-03 03:30:44 -0400

    Fix linter errors

--- src/pages/MyGames.tsx
+++ src/pages/MyGames.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useAuth } from '../hooks/useAuth';
